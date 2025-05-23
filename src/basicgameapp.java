8983b608e4101149630a3a87a08a45e10cc79400
leifangelini1 <leif_angelini27@milton.edu>
2025-05-23 08:42:37 -0400

    h

--- src/basicgameapp.java
+++ src/basicgameapp.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class basicgameapp {
     public player kat;
     public dealer lizard;
-    public card [] deck = new card[51];
+    public card [] deck = new card[52];
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         new basicgameapp();
@@ -11,17 +11,29 @@ public class basicgameapp {
     }
     public basicgameapp() {
         System.out.println("welcome to mr. angelini's blackjack game");
+        int counter =0;
         for (int l = 0;l<4;l++) {
-            int y =0;
 
             for (int x = 0; x < 13; x++) {
-                card c = new card(x + 1,y);
-                c.printall();
+                card c = new card(x + 1,l);
 
-                c = deck[x];
-                y++;
+                deck[counter] = c;
+                deck[counter].printall();
+                counter++;
+
+
+
             }
 
         }
+        printdeck();
     }
+        public void printdeck(){
+            for(int x = 0; x<deck.length; x++){
+                System.out.println(x);
+                deck[x].printall();
+
+            }
+        }
+
 }
--- src/player.java
+++ src/player.java
@@ -1,7 +1,6 @@
-import java.sql.Array;
-
 public class player {
     public int cardtotal;
     boolean ishit;
     public card [] cards;
+    public player sammy = new player();
 }
