57596aae8241683d59f36579fce741081f3b098b
leifangelini1 <leif_angelini27@milton.edu>
2025-05-27 10:38:20 -0400

    h

--- src/dealer.java
+++ src/dealer.java
@@ -2,4 +2,15 @@ public class dealer {
     public int carttotal;
     public card [] cards;
     public boolean isover17;
+    public dealer(){
+        carttotal = 1;
+        isover17 = true;
+
+    }
+
+    public void printall(){
+        System.out.println(carttotal);
+        System.out.println(cards);
+        System.out.println(isover17);
+    }
 }
--- src/player.java
+++ src/player.java
@@ -3,4 +3,16 @@ public class player {
     boolean ishit;
     public card [] cards;
     public player sammy = new player();
+
+    public player() {
+        cardtotal = 1;
+        ishit = true;
+
+    }
+    public void pintall(){
+        System.out.println(cardtotal);
+        System.out.println(ishit);
+        System.out.println(cards);
+        System.out.println(sammy);
+    }
 }
